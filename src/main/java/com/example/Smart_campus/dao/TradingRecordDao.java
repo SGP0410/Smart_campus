2eeda16cce8dc60cc86a3a8c67e538ff3979d3fd
SGP0410 <2742837998@qq.com>
2021-08-10 00:19:54 +0800

    2021-8-10 0:16

--- src/main/java/com/example/Smart_campus/dao/TradingRecordDao.java
+++ src/main/java/com/example/Smart_campus/dao/TradingRecordDao.java
@@ -5,10 +5,25 @@ import java.util.List;
 
 public interface TradingRecordDao {
 
+    /**
+     * 更具校园卡查询消费记录
+     * @param schoolCardId
+     * @return
+     */
     public List<TradingRecord> queryTradingRecordBySchoolCardId(String schoolCardId);
 
+    /**
+     * 添加消费记录
+     * @param tradingRecord
+     * @return
+     */
     public int addTradingRecord(TradingRecord tradingRecord);
 
+    /**
+     * 更具消费记录id删除消费记录
+     * @param tradingRecordId
+     * @return
+     */
     public int deleteTradingRecordById(String tradingRecordId);
 
 }
---
+++ src/main/java/com/example/Smart_campus/servlet/GetTradingRecordBySchoolCardId.java
@@ -0,0 +1,43 @@
+package com.example.Smart_campus.servlet;
+
+import com.example.Smart_campus.bean.TradingRecord;
+import com.example.Smart_campus.dao.impl.TradingRecordDaoImpl;
+import com.example.Smart_campus.utils.ServletUtils;
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "GetTradingRecordBySchoolCardId", value = "/GetTradingRecordBySchoolCardId")
+public class GetTradingRecordBySchoolCardId extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletUtils.Setting(request, response);
+        JSONObject jsonObject = ServletUtils.getJSONObject(request);
+
+        List<TradingRecord> tradingRecordList = new TradingRecordDaoImpl()
+                .queryTradingRecordBySchoolCardId(jsonObject.optString("schoolCardId"));
+
+        JSONObject jsonObject1 = new JSONObject();
+
+        if (tradingRecordList != null){
+            JSONArray jsonArray = new JSONArray(new Gson().toJson(tradingRecordList));
+            jsonObject1.put("rows" , jsonArray);
+        }
+
+        ServletUtils.isOk(jsonObject1 , tradingRecordList != null);
+
+        response.getWriter().write(jsonObject.toString());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
